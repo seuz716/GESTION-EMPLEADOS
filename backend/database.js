f3f75a9cec8bdb979a6247c8e62392bbbc848afb
seuz716 <ceanabad@gmail.com>
2023-08-28 14:57:31 -0500

    base de datos conectada exitosamente

--- backend/database.js
+++ backend/database.js
@@ -1,33 +1,17 @@
 const mongoose = require('mongoose');
 
-// URL de conexión a la base de datos
-const dbURI = 'mongodb+srv://cesar:cesar@cluster0.wtlfm.mongodb.net/empleados';
+// Conexión a la base de datos MongoDB a través de Mongoose
+mongoose.connect('mongodb+srv://cesar:cesar@cluster0.wtlfm.mongodb.net/', {
+  useNewUrlParser: true, // Usar el nuevo analizador de URL
+  useUnifiedTopology: true, // Usar el nuevo motor de topología unificada
+})
+.then(() => {
+  console.log('Conexión a la base de datos exitosa'); // Mensaje si la conexión es exitosa
+})
+.catch(error => {
+  console.error('Error en la conexión a la base de datos:', error); // Mensaje si hay un error en la conexión
+});
 
-// Conectando a la base de datos
-mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch(error => {
-    console.error('Database connection error:', error);
-  });
-
-// Manejar eventos de conexión
-const dbConnection = mongoose.connection;
-dbConnection.on('connected', () => {
-  console.log('Connected to the MongoDB Atlas cluster');
-});
-dbConnection.on('error', error => {
-  console.error('MongoDB Atlas connection error:', error);
-});
-dbConnection.on('disconnected', () => {
-  console.log('Disconnected from the MongoDB Atlas cluster');
-});
 
 // Exportar la instancia de mongoose para reutilización
 module.exports = mongoose;
