d62123dee33c74382bddadb09a28000d5c8210d5
seuz716 <ceanabad@gmail.com>
2023-09-01 12:45:54 -0500

    aplicacion lista

--- fronted/src/app/components/empleados/empleados.component.ts
+++ fronted/src/app/components/empleados/empleados.component.ts
@@ -1,37 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { EmpleadoService } from '../../services/empleado.service';
-import { NgForm } from '@angular/forms';
-import { Empleado } from '../models/empleado';
+import { Component, OnInit } from '@angular/core'; // Cambié las comillas
+
+import { EmpleadoService } from '../../services/empleado.service'; // Cambié las comillas
+
+import { NgForm } from '@angular/forms'; // Cambié las comillas
+
+import { Empleado } from '../models/empleado'; // Cambié las comillas
 
 declare var M: any;
 
 @Component({
+
   selector: 'app-empleados',
+
   templateUrl: './empleados.component.html',
+
   styleUrls: ['./empleados.component.css'],
+
   providers: [EmpleadoService]
+
 })
+
 export class EmpleadosComponent implements OnInit {
 
   constructor(public empleadoService: EmpleadoService) { }
 
   ngOnInit(): void {
-    // Aquí podrías realizar inicializaciones si es necesario
+
   }
 
   agregarEmpleado(form?: NgForm) {
-    if (form) {
-      this.empleadoService.postEmpleado(form.value).subscribe(res => {
+
+    this.empleadoService.PostEmpleado(form?.value)
+
+      .subscribe(res => {
+
         this.resetForm(form);
+
         M.toast({ html: 'Guardado satisfactoriamente' });
+
       });
-    }
+
   }
 
-  resetForm(form?: NgForm) {
+  resetForm(form?: NgForm) { // Limpiar el formulario, recibe un formulario como parámetro
+
     if (form) {
+
       form.reset();
+
       this.empleadoService.selectedEmpleado = new Empleado();
+
     }
+
   }
+
 }
--- fronted/src/app/services/empleado.service.ts
+++ fronted/src/app/services/empleado.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Empleado } from '../components/models/empleado';
+import { Injectable } from '@angular/core'; // Cambié las comillas
+
+import { HttpClient } from '@angular/common/http'; // Cambié las comillas
+
+import { Empleado } from '../components/models/empleado' ; // Cambié las comillas
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EmpleadoService {
+
   selectedEmpleado: Empleado;
   empleados: Empleado[];
   readonly URL_API = 'http://localhost:3000/api/empleados';
@@ -19,11 +23,11 @@ export class EmpleadoService {
     return this.http.get(this.URL_API);
   }
 
-  postEmpleado(empleado: Empleado) {
-    return this.http.post(this.URL_API, empleado);
+  PostEmpleado(empleado: Empleado) { // Cambié el nombre del parámetro a "empleado"
+    return this.http.post(this.URL_API, empleado); // Cambié el nombre del parámetro a "empleado"
   }
 
-  putEmpleado(empleado: Empleado) {
+  putEmpleado(empleado: Empleado) { // Cambié el nombre del parámetro a "empleado"
     return this.http.put(`${this.URL_API}/${empleado._id}`, empleado);
   }
 
